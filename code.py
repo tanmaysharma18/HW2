b9f2753ae0ec3a5fced4a6136e02f0145f6d5bd7
tanmaysharma786 <64467958+tanmaysharma786@users.noreply.github.com>
2020-05-29 23:27:24 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[49]:
+
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.datasets import mnist
+from keras.utils import np_utils
+from keras.optimizers import Adam
+
+# loads the MNIST dataset
+(x_train, y_train), (x_test, y_test)  = mnist.load_data()
+
+# Lets store the number of rows and columns
+img_rows = x_train[0].shape[0]
+img_cols = x_train[1].shape[0]
+
+# Getting our date in the right 'shape' needed for Keras
+# We need to add a 4th dimenion to our date thereby changing our
+# Our original image shape of (60000,28,28) to (60000,28,28,1)
+x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+
+# store the shape of a single image 
+input_shape = (img_rows, img_cols, 1)
+
+# change our image type to float32 data type
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+
+# Normalize our data by changing the range from (0 to 255) to (0 to 1)
+#x_train /= 255
+#x_test /= 255
+
+
+# In[50]:
+
+
+# Now we one hot encode outputs
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+
+num_classes = y_test.shape[1]
+
+# create model
+model = Sequential()
+
+# sets of CRP (Convolution, RELU, Pooling)
+model.add(Conv2D(30, (5, 5),
+                 padding = "same", 
+                 input_shape = input_shape))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size = (2, 2)))
+
+model.add(Conv2D(30, (5, 5),
+                 padding = "same", 
+                 input_shape = input_shape))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size = (2, 2)))
+
+
+
+
+# Fully connected layers (w/ RELU)
+model.add(Flatten())
+model.add(Dense(units=20, activation='relu'))
+     
+
+# Softmax (for classification)
+model.add(Dense(num_classes))
+model.add(Activation("softmax"))
+           
+model.compile(loss = 'categorical_crossentropy', optimizer = Adam(), metrics = ['accuracy'])
+    
+print(model.summary())
+
+
+# In[51]:
+
+
+batch_size = 128
+epochs = 1
+
+history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(x_test, y_test), shuffle=True)
+
+
+# Evaluate the performance of our trained model
+scores = model.evaluate(x_test, y_test, verbose=1)
+print('Test loss:', scores[0])
+print('Test accuracy:', scores[1])
+
+
+# In[53]:
+
+
+print(scores[1])
+
+
+# In[47]:
+
+
+str(int(scores[1]))
+
+
+# In[48]:
+
+
+file = open('ak.txt', 'w')
+file.write(str(int(scores[1])))
+
+
+# In[45]:
+
+
+scores[1]
+
+
+# In[ ]:
+
+
+
+
